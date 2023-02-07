c4334c298dd2f1d2020fc6d24e4df70a69959299
Jonathan-Gaal <jonathangaal@pursuit.org>
2023-02-07 13:01:41 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+// DEPENDENCIES
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+
+const artController = require("./controllers/artController");
+
+// CONFIGURATION
+
+// MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use("/art", artController);
+
+// ROUTES
+app.get("/", (req, res) => {
+  res.send("Welcome to Jons Art");
+});
+
+// 404 PAGE
+app.get("*", (req, res) => {
+  res.status(404).send("Page not found");
+});
+
+// EXPORT
+module.exports = app;
---
+++ controllers/artController.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const { all } = require("../app");
+const gallery = express.Router();
+const {
+  getAllArtwork,
+  getOneArtwork,
+  createArtwork,
+  deleteArtwork,
+  updateArtwork,
+} = require("../queries/artwork");
+const { checkTitle, validateURL } = require("../validations/checkArt");
+
+const commentsController = require("./commentsController");
+gallery.use("/:artId/comments", commentsController);
+
+// INDEX/GALLERY
+gallery.get("/", async (req, res) => {
+  const allArtWork = await getAllArtwork();
+  if (allArtwork[0]) {
+    res.status(200).json(allArtwork);
+  } else {
+    res.status(500).json({ error: allArtWork.message });
+  }
+});
+
+// GET ONE ARTWORK
+gallery.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const song = await getOneArtwork(id);
+  if (!artwork.message) {
+    res.json(artwork);
+  } else {
+    res.status(404).json({ error: artwork.message });
+  }
+});
+
+gallery.post("/", async (req, res) => {
+  try {
+    const artwork = await createArtwork(req.body);
+    res.json(artwork);
+  } catch (err) {
+    res.status(400).json({ error: artwork.message });
+  }
+});
+
+gallery.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedArtwork = await deleteArtwork(id);
+    if (deletedArtwork.id) {
+      res.status(200).json(deletedArtwork);
+    }
+  } catch (err) {
+    res.status(404).json({ error: deleteArtwork.message });
+  }
+});
+
+gallery.put("/:id", checkTitle, checkBoolean, validateURL, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedSong = await updateSong(id, req.body);
+    res.status(200).json(updatedSong);
+  } catch (err) {
+    res.status(404).json({ error: song.message });
+  }
+});
+
+module.exports = songs;
