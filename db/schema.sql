781fbdb41f85a2be0e09bb386a1c8a5e4add3220
Jonathan-Gaal <jonathangaal@pursuit.org>
2023-08-23 16:55:15 -0400

    adding customer tables

--- db/schema.sql
+++ db/schema.sql
@@ -5,6 +5,7 @@ CREATE DATABASE jonsArt_dev;
 
 DROP TABLE IF EXISTS comments;
 DROP TABLE IF EXISTS gallery;
+-- DROP TABLE IF EXISTS shoppingCart;
 
 CREATE TABLE gallery (
     id SERIAL PRIMARY KEY,
@@ -20,7 +21,16 @@ CREATE TABLE gallery (
     depth TEXT
 );
 
-DROP TABLE IF EXISTS comments;
+--TODO: remove this statement if it works without it
+-- DROP TABLE IF EXISTS comments;
+
+-- CREATE TABLE users (
+--  user_id SERIAL PRIMARY KEY,
+--  user_firstName VARCHAR(100),
+--  user_lastName VARCHAR(100),
+--  user_email VARCHAR(100),
+--  user_passWord VARCHAR(100),
+-- );
 
 CREATE TABLE comments (
  id SERIAL PRIMARY KEY,
@@ -28,5 +38,28 @@ CREATE TABLE comments (
  comment TEXT,
  post_date VARCHAR(10),
  art_id INTEGER REFERENCES gallery (id)
+ user_id INTEGER REFERENCES users (user_id)
  ON DELETE CASCADE
 );
+
+-- CREATE TABLE userPaymentMethods (
+--  paymentMethod_id SERIAL PRIMARY KEY,
+-- creditCard_number VARCHAR(16)
+-- creditCard_expireDate VARCHAR(4)
+-- creditCard_securityCode VARCHAR(4)
+-- user_id INTEGER REFERENCES users (user_id)
+--  ON DELETE CASCADE
+-- );
+
+
+-- CREATE TABLE shoppingCart (
+--  shoppingCart_id SERIAL PRIMARY KEY,
+--  item_id INTEGER REFERENCES gallery (id)
+--  user_id INTEGER REFERENCES users (id)
+--  ON DELETE CASCADE
+-- );
+
+
+
+
+
